35b806f2a76953f2a9346e8be0fe3a45b8eea919
rushabhporwal29 <45138812+rushabhporwal29@users.noreply.github.com>
2020-11-25 15:09:03 +0530

    inserted count & student in attendance.db

--- Insert.py
+++ Insert.py
@@ -26,8 +26,31 @@ def insert_student():
         csv_reader = csv.reader(file)
         i = 0
         for rows in csv_reader:
-            print(rows[1],rows[2])         
+            print(rows[1],rows[2],rows[3])         
             if i==0 :
                 i=2 
+            else:
+                 cursor.execute(""" 
+                        
+                        insert into student (Name,roll,cid) VALUES (?,?,?);
+                        """,(rows[1],rows[2],rows[3]))
+
+def insert_LectureCount():
+    with open ("Data\LectureCount.csv",'r') as file:
+        csv_reader = csv.reader(file)
+        i = 0
+        for rows in csv_reader:
+            print(rows[0],rows[1],rows[2],rows[3],rows[4])         
+            if i==0 :
+                i=2 
+            else:
+                 cursor.execute(""" 
+                        
+                        insert into lec_count (year,subject,a,b,c) VALUES (?,?,?,?,?);
+                        """,(rows[0],rows[1],rows[2],rows[3],rows[4]))
+
+
+# insert_student()
+insert_LectureCount()
 conn.commit()
 conn.close()
