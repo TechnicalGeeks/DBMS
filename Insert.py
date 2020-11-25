6b4be14293fa0683997f64da78d1d8b008e86b5a
rushabhporwal29 <45138812+rushabhporwal29@users.noreply.github.com>
2020-11-25 14:49:50 +0530

    Created csv files in data

--- Insert.py
+++ Insert.py
@@ -21,5 +21,13 @@ def   insert_class():
                         insert into class (Year,Div) VALUES (?,?);
                         """,(rows[1],rows[2]))
 
+def insert_student():
+    with open ("Data\Student.csv",'r') as file:
+        csv_reader = csv.reader(file)
+        i = 0
+        for rows in csv_reader:
+            print(rows[1],rows[2])         
+            if i==0 :
+                i=2 
 conn.commit()
 conn.close()
